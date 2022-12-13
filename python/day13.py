046e9bb4619d7feaa47b8dc6f4832e3aaaa35fcb
ntaheij <noah@taheij.nl>
2022-12-13 08:12:50 +0100

    Day 13

---
+++ python/day13.py
@@ -0,0 +1,26 @@
+def in_order(l1, l2):
+    if isinstance(l1, int) and isinstance(l2, int):
+        if l1 == l2:
+            return None
+        return l1 < l2
+
+    if isinstance(l1, list) and isinstance(l2, list):
+        for e1, e2 in zip(l1, l2):
+            if (comparison := in_order(e1, e2)) is not None:
+                return comparison
+        return in_order(len(l1), len(l2))
+
+    if isinstance(l1, int):
+        return in_order([l1], l2)
+    return in_order(l1, [l2])
+
+
+if __name__ == "__main__":
+    input = open('inputs/input.txt').read()
+    pairs = [[eval(l) for l in pair.splitlines()]for pair in input.strip().split("\n\n")]
+    print(sum(i for i, (left, right) in enumerate(pairs, 1) if in_order(left, right)))
+
+    packets = [p for pair in pairs for p in pair]
+    position_1 = 1 + sum(1 for p in packets if in_order(p, [[2]]))
+    position_2 = 2 + sum(1 for p in packets if in_order(p, [[6]]))
+    print(position_1 * position_2)
